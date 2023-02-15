23f3337fa2373d4773f55ca7c0e3af1f7eb626cd
SANTHOSH REKANAR <santhoshrekanar@gmail.com>
2023-02-15 11:48:49 +0530

    new changes for custom binding added (#5)

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,13 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // name = "Max";
-  showSecret = false;
-  log = [];
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    // this.log.push(this.log.length + 1);
-    this.log.push(new Date());
+  serverElements = [{type:'server', name:'TestServer', content:'Just a test!'}];
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
   }
+  onBluePrintAdded(bluePrintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+   }
+
+  // servers = [];
+  // name = "Max";
+  // showSecret = false;
+  // log = [];
+
+  // onToggleDetails() {
+  //   this.showSecret = !this.showSecret;
+  //   // this.log.push(this.log.length + 1);
+  //   this.log.push(new Date());
+  // }
+//   onAddServer() {
+//     this.servers.push('Another Server');
+//   }
+//   onRemoteServer(id: number)
+//   {
+//
+//     const position = id + 1;
+//     this.servers.splice(position, 1);
+//    }
+
+
 }
